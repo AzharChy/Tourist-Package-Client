1f3d2ca1d61b1b61fec8fed72337c1484499c630
AzharChy <azharChowdhury075@gmail.com>
2025-06-08 13:19:46 +0600

    auhtentication complete

--- src/Routes/Pages/Authentication/AuthProvider.jsx
+++ src/Routes/Pages/Authentication/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase.config';
 import { AuthContext } from './AuthContext';
 
@@ -7,7 +7,7 @@ import { AuthContext } from './AuthContext';
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState();
     const [loading, setLoading] = useState(true);
- 
+   const provider = new GoogleAuthProvider();
 
     // create user with email and apssword
     const createUser = (email,password) =>{
@@ -16,11 +16,41 @@ const AuthProvider = ({children}) => {
 
     // login wit hemail and apss
 
+    const loginUser = (email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    // login via google
+
+    const loginWithGoogle = ()=>{
+        return signInWithPopup(auth, provider)
+    }
+
+    // logout
+
+    const logout = () =>{
+        return signOut(auth);
+    }
+
+    // obserever 
+
+   useEffect(()=>{
+     const unSubscribe =  onAuthStateChanged(auth, (currentUser)=>{
+        setUser(currentUser);
+        setLoading(false);
+     })
+    }
+   ,[])
+
     const authData = {
         user,
         setUser,
         createUser,
-        loading
+        loading,
+       
+        logout,
+        loginWithGoogle,
+        loginUser
     }
     return (
        <AuthContext.Provider value={authData}>
